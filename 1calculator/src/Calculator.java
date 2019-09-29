efebd154aeedb0b9c687a43d52e8ecf99c9cdd79
u.malichenko <u.malichenko@gmail.com>
2019-09-29 22:43:52 +0300

    1

---
+++ 1calculator/src/Calculator.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main (String  [] args){
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Введите операцию [+, -, /, *]: ");
+        String operation = userInput.nextLine();
+
+        System.out.println("Введите первый аргумент: ");
+        double argument1 = userInput.nextDouble();
+
+        System.out.println("Введите второй аргумент: ");
+        double argument2 = userInput.nextDouble();
+        userInput.close();
+
+        switch (operation){
+            case "+":
+                System.out.println(argument1 + argument2);
+                break;
+            case "-":
+                System.out.println(argument1 - argument2);
+                break;
+            case "/":
+                System.out.println(argument1 / argument2);
+                break;
+            case "*":
+                System.out.println(argument1 * argument2);
+                break;
+            default:
+                System.err.println("Нет такой операции");
+                break;
+        }
+    }
+}
