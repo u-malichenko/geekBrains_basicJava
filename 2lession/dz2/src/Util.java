2addf0ed7bdd1aa98f549904c1aa7cacf39a6268
u.malichenko <u.malichenko@gmail.com>
2019-10-04 12:52:25 +0300

    finish v1

---
+++ 2lession/dz2/src/Util.java
@@ -0,0 +1,27 @@
+public class Util {
+    //метод для заполнения одномерного массива рандомными числами:
+    static void getArr(int n, int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] =  (int) (Math.random() * n);
+        }
+    }
+
+    // метод для печати одномерного массива:
+    static void printArr(int[] arr, String comment){
+        for (int value : arr) {
+            System.out.print(value + "\t");
+        }
+        System.out.println(comment);
+    }
+
+    // метод для печати квадратного массива
+    static void printArr(int[][] arr){
+        for (int i = 0; i < arr.length ; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+}
