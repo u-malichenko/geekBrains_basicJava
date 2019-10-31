ae8521c06dc74e727ffc14eb13157bc4cff73e80
u.malichenko <u.malichenko@gmail.com>
2019-10-31 09:58:54 +0300

    6

--- Lession7/src/game/GamePlayer.java
+++ Lession7/src/game/GamePlayer.java
@@ -9,6 +9,17 @@ public class GamePlayer {
     //признак рельный игрок или пк
     private boolean realPlayer = true;
 
+    //запрашиваем знак ПК
+    private char DOT_O = getPlayerSign();
+    //запрашиваем размерность игрового поля
+    private int SIZE = GameBoard.dimension;
+
+    //начальные заведомо ошибочные координаты
+    private int row = -1;
+    private int cell = -1;
+    Random rnd = new Random();
+
+
     /**
      * Конструктор игрока
      * @param isRealPlayer - признак игрок или компьютер
@@ -69,155 +80,34 @@ public class GamePlayer {
         }
     }
 
+    /**
+     * ход компьютера
+     * @param button
+     */
     void updateByAiData(GameButton button) {
         //генерация координат хода компьютера
-        int row = -1, cell = -1;
-        Random rnd = new Random();
-
-        //проверяем играет ли ПК в роли рандомного пк?
+        //проверяем играет ли ПК в роли рандомного символа(isSillyMode)?
         if (button.getBoard().getGame().isSillyMode()) {
             do {
                 row = rnd.nextInt(GameBoard.dimension);
                 cell = rnd.nextInt(GameBoard.dimension);
             } while (!button.getBoard().isTurnable(row, cell));
         }
-
         //иначе ПК игрет в роли сильного ПК
+        // (есть два способа
+        // 1 подбор по ближайшему полю содержащему 0(isScoringMode = true)
+        // 2 взвешивание каждой ячейки и определение самой подходящей
         else {
-            //запрашиваем знак ПК
-            char DOT_O = button.getBoard().getGame().getCurrentPlayer().getPlayerSign();
-            //запрашиваем размерность игрового поля
-            int SIZE = button.getBoard().getDimension();
-
-            //компьютер ищет свой символ
-            //иначе компьютер решает, можно ли продолжить последовательность для выигрыша
-
-            //проверяем играет ПК просто подбирая соседние ячейки или хочет рассчитывать вес ячеек?
-//true - выключен режим простого подюора!
+            //проверяем играет ПК просто подбирая соседние ячейки или хочет рассчитывать вес ячеек? isScoringMode = true - выключен режим простого подбора!
             if (button.getBoard().getGame().isScoringMode()) {
-                //маркер торго, что ход найден! Нужен для выхода из двойного цикла
-                boolean moveFound = false;
-                //упрощенный алгоритм
-                for (int i = 0; i < SIZE; i++) { // строки
-                    for (int j = 0; j < SIZE; j++) { // столбцы       обходим массив двойным циклом
-                        if (button.getBoard().isTurnable(i, j)) { //если мы в текущий момент работаем с пустой клеткой то начинаем опеределять соседей
-                            //проверяем направления
-                            //лево верх
-                            if (i - 1 >= 0 && j - 1 >= 0 && button.getBoard().getGameField(i - 1, j - 1) == DOT_O) {
-                                // проверяем больше ли они нуля, чтоб не выходить за границы массива? последним условием проверяем что там есть знак ПК
-                                row = i; // строки
-                                cell = j; // столбцы
-                                moveFound = true;
-                                System.out.println("LU");
-                            }
-                            //верх
-                            else if (i - 1 >= 0 && button.getBoard().getGameField(i - 1, j) == DOT_O) {
-                                row = i;
-                                cell = j;
-                                moveFound = true;
-                                System.out.println("U");
-                            }
-                            //право верх
-                            else if (i - 1 >= 0 && j + 1 < SIZE && button.getBoard().getGameField(i - 1, j + 1) == DOT_O) {
-                                row = i;
-                                cell = j;
-                                moveFound = true;
-                                System.out.println("RU");
-                            }
-                            //право
-                            else if (j + 1 < SIZE && button.getBoard().getGameField(i, j + 1) == DOT_O) {
-                                row = i;
-                                cell = j;
-                                moveFound = true;
-                                System.out.println("R");
-                            }
-                            //право низ
-                            else if (i + 1 < SIZE && j + 1 < SIZE && button.getBoard().getGameField(i + 1, j + 1) == DOT_O) {
-                                row = i;
-                                cell = j;
-                                moveFound = true;
-                                System.out.println("RD");
-                            }
-                            //низ
-                            else if (i + 1 < SIZE && button.getBoard().getGameField(i + 1, j) == DOT_O) {
-                                row = i;
-                                cell = j;
-                                moveFound = true;
-                                System.out.println("B");
-                            }
-                            //лево низ
-                            else if (i + 1 < SIZE && j - 1 >= 0 && button.getBoard().getGameField(i + 1, j - 1) == DOT_O) {
-                                row = i;
-                                cell = j;
-                                moveFound = true;
-                                System.out.println("LB");
-                            }
-                            //лево
-                            else if (j - 1 >= 0 && button.getBoard().getGameField(i, j - 1) == DOT_O) {
-                                row = i;
-                                cell = j;
-                                moveFound = true;
-                                System.out.println("L");
-                            }
-                        }
-                        //если ход найден, прерываем внутренний цикл
-                        if (moveFound) break;
-                    }
-                    //если ход найден, прерываем внешинй цикл
-                    if (moveFound) break;
-                }
-            }
-            // конец простого хода пк
+                // сложный режим 1 c одбором, вынес в отдельный метод scoringModeOn
+                scoringModeOn(button);
+            } // конец сложного хода с обычным подбором ячеек содержащих 0
             else {
-// сложный режим
-                int maxScoreFieldRow = -1;
-                int maxScoreFieldCell = -1;
-                int maxScore = 0; // максимальный рейтинг
-
-                for (int i = 0; i < SIZE; i++) { //первый цикл строк
-                    for (int j = 0; j < SIZE; j++) { //второй цикл столбцов
-                        int fieldScore = 0; //переменная для текущего значения рейтинга
-
-                        if (button.getBoard().isTurnable(i, j)) { // если поле пустое? то проверяем направления для этой ячейки и считаем рейтинг
-                            // проверяем направления
-                            //лево верх
-                            if (i - 1 >= 0 && j - 1 >= 0 && button.getBoard().getGameField(i - 1, j - 1) == DOT_O)
-                                fieldScore++;
-                            //верх
-                            if (i - 1 >= 0 && button.getBoard().getGameField(i - 1, j) == DOT_O) fieldScore++;
-                            //право верх
-                            if (i - 1 >= 0 && j + 1 < SIZE && button.getBoard().getGameField(i - 1, j + 1) == DOT_O)
-                                fieldScore++;
-                            //право
-                            if (j + 1 < SIZE && button.getBoard().getGameField(i, j + 1) == DOT_O) fieldScore++;
-                            //право низ
-                            if (i + 1 < SIZE && j + 1 < SIZE && button.getBoard().getGameField(i + 1, j + 1) == DOT_O)
-                                fieldScore++;
-                            //низ
-                            if (i + 1 < SIZE && button.getBoard().getGameField(i + 1, j) == DOT_O) fieldScore++;
-                            //лево низ
-                            if (i + 1 < SIZE && j - 1 >= 0 && button.getBoard().getGameField(i + 1, j - 1) == DOT_O)
-                                fieldScore++;
-                            //лево
-                            if (j - 1 >= 0 && button.getBoard().getGameField(i, j - 1) == DOT_O) fieldScore++;
-                        } // конец проверки ПУСТОЙ ячейки
-
-                        if (fieldScore > maxScore) { //сравниваем текущее полученное колличество очков с предыдущим максимальным
-                            maxScore = fieldScore; //, если надо заменяем максимальное и координаты
-                            maxScoreFieldRow = i;
-                            maxScoreFieldCell = j;
-                        }
-                    }//первый фор
-                }//второй фор
-
-                //если в цикле найдена наилучшая клетка
-                if (maxScoreFieldRow != -1) {
-                    row = maxScoreFieldRow; //обновим координаты х у через временные из цикла
-                    cell = maxScoreFieldCell;
-                }
+                // сложный режим 2, вынес в отдельный метод scoringModeOff
+                scoringModeOff(button);
             } // конец сложного хода конец эльзе
-
-            //если ни чего не нашли, тогда генерируем случайный ход
+            //если ни чего не нашли, тогда генерируем случайный ход, или для первого хода
             if (row == -1) {
                 do {
                     row = rnd.nextInt(GameBoard.dimension);
@@ -225,33 +115,154 @@ public class GamePlayer {
                 } while (!button.getBoard().isTurnable(row, cell));
             }
         }
-
         //обновить матрицу игры
         button.getBoard().updateGameField(row, cell); //строку и столбец
-
         //обновить содержимое кнопки
         //получить индекс кнопки:
         //размерность нашего поля нужно умножить на номер ряда и прибавить номер столбца
         //это обратная формула к тем самым двум
         int cellIndex = GameBoard.dimension * row + cell;
-
         //получить кнопку по ее индексу getButton(cellIndex
         //и присваиваем ее тексту значение символа текущего игрока тоесть компа
         button.getBoard().getButton(cellIndex).setText(Character.toString(button.getBoard().getGame().getCurrentPlayer().getPlayerSign()));
-
         //проверяем победу
         if (button.getBoard().checkWin()) {
             button.getBoard().getGame().showMessage("Компьютер выиграл");
             button.getBoard().emptyField();
-
-//Даж еесли ПК, каким то чудом выиграл, все равно нужно предавать ход живому игроку
-//иначе первый ход игрока будет с 0 вместо крестика!
+            //Даж еесли ПК, каким то чудом выиграл, все равно нужно предавать ход живому игроку
+            //иначе первый ход игрока будет с 0 вместо крестика!
             button.getBoard().getGame().passTurn();
-
-        } else {//если победа не найдена то предаем ход игроку
+        } else {
+            //если победа не найдена то предаем ход игроку
             //передать ход
             button.getBoard().getGame().passTurn();
         }
+    }
 
+    /**
+     * метод ложного ПК 1 - включен режим простого подбора ячеек по первому совпадению со знаком ПК
+     * @param button- ссылка на кнопку для получения  свойств
+     */
+    private void scoringModeOn (GameButton button){
+        //маркер торго, что ход найден! Нужен для выхода из двойного цикла
+        boolean moveFound = false;
+        //упрощенный алгоритм
+        for (int i = 0; i < SIZE; i++) { // строки
+            for (int j = 0; j < SIZE; j++) { // столбцы       обходим массив двойным циклом
+                if (button.getBoard().isTurnable(i, j)) { //если мы в текущий момент работаем с пустой клеткой то начинаем опеределять соседей
+                    //проверяем направления
+                    //лево верх
+                    if (i - 1 >= 0 && j - 1 >= 0 && button.getBoard().getGameField(i - 1, j - 1) == DOT_O) {
+                        // проверяем больше ли они нуля, чтоб не выходить за границы массива? последним условием проверяем что там есть знак ПК
+                        row = i; // строки
+                        cell = j; // столбцы
+                        moveFound = true;
+                        System.out.println("LU");
+                    }
+                    //верх
+                    else if (i - 1 >= 0 && button.getBoard().getGameField(i - 1, j) == DOT_O) {
+                        row = i;
+                        cell = j;
+                        moveFound = true;
+                        System.out.println("U");
+                    }
+                    //право верх
+                    else if (i - 1 >= 0 && j + 1 < SIZE && button.getBoard().getGameField(i - 1, j + 1) == DOT_O) {
+                        row = i;
+                        cell = j;
+                        moveFound = true;
+                        System.out.println("RU");
+                    }
+                    //право
+                    else if (j + 1 < SIZE && button.getBoard().getGameField(i, j + 1) == DOT_O) {
+                        row = i;
+                        cell = j;
+                        moveFound = true;
+                        System.out.println("R");
+                    }
+                    //право низ
+                    else if (i + 1 < SIZE && j + 1 < SIZE && button.getBoard().getGameField(i + 1, j + 1) == DOT_O) {
+                        row = i;
+                        cell = j;
+                        moveFound = true;
+                        System.out.println("RD");
+                    }
+                    //низ
+                    else if (i + 1 < SIZE && button.getBoard().getGameField(i + 1, j) == DOT_O) {
+                        row = i;
+                        cell = j;
+                        moveFound = true;
+                        System.out.println("B");
+                    }
+                    //лево низ
+                    else if (i + 1 < SIZE && j - 1 >= 0 && button.getBoard().getGameField(i + 1, j - 1) == DOT_O) {
+                        row = i;
+                        cell = j;
+                        moveFound = true;
+                        System.out.println("LB");
+                    }
+                    //лево
+                    else if (j - 1 >= 0 && button.getBoard().getGameField(i, j - 1) == DOT_O) {
+                        row = i;
+                        cell = j;
+                        moveFound = true;
+                        System.out.println("L");
+                    }
+                }
+                //если ход найден, прерываем внутренний цикл
+                if (moveFound) break;
+            }
+            //если ход найден, прерываем внешинй цикл
+            if (moveFound) break;
+        }
+    }
+
+    /**
+     * метод сложного пк 2 - взвешивает ценность каждой кнопки
+     * @param button - ссылка на кнопку для получения  свойств
+     */
+    private void scoringModeOff(GameButton button){
+        int maxScoreFieldRow = -1;
+        int maxScoreFieldCell = -1;
+        int maxScore = 0; // максимальный рейтинг
+
+        for (int i = 0; i < SIZE; i++) { //первый цикл строк
+            for (int j = 0; j < SIZE; j++) { //второй цикл столбцов
+                int fieldScore = 0; //переменная для текущего значения рейтинга
+                if (button.getBoard().isTurnable(i, j)) { // если поле пустое? то проверяем направления для этой ячейки и считаем рейтинг
+                    // проверяем направления
+                    //лево верх
+                    if (i - 1 >= 0 && j - 1 >= 0 && button.getBoard().getGameField(i - 1, j - 1) == DOT_O)
+                        fieldScore++;
+                    //верх
+                    if (i - 1 >= 0 && button.getBoard().getGameField(i - 1, j) == DOT_O) fieldScore++;
+                    //право верх
+                    if (i - 1 >= 0 && j + 1 < SIZE && button.getBoard().getGameField(i - 1, j + 1) == DOT_O)
+                        fieldScore++;
+                    //право
+                    if (j + 1 < SIZE && button.getBoard().getGameField(i, j + 1) == DOT_O) fieldScore++;
+                    //право низ
+                    if (i + 1 < SIZE && j + 1 < SIZE && button.getBoard().getGameField(i + 1, j + 1) == DOT_O)
+                        fieldScore++;
+                    //низ
+                    if (i + 1 < SIZE && button.getBoard().getGameField(i + 1, j) == DOT_O) fieldScore++;
+                    //лево низ
+                    if (i + 1 < SIZE && j - 1 >= 0 && button.getBoard().getGameField(i + 1, j - 1) == DOT_O)
+                        fieldScore++;
+                    //лево
+                    if (j - 1 >= 0 && button.getBoard().getGameField(i, j - 1) == DOT_O) fieldScore++;
+                } // конец проверки ПУСТОЙ ячейки
+                if (fieldScore > maxScore) { //сравниваем текущее полученное колличество очков с предыдущим максимальным
+                    maxScore = fieldScore; //, если надо заменяем максимальное и координаты
+                    maxScoreFieldRow = i;
+                    maxScoreFieldCell = j;
+                }
+            }//первый фор
+        }//второй фор
+        //если в цикле найдена наилучшая клетка
+        if (maxScoreFieldRow != -1) {
+            row = maxScoreFieldRow; //обновим координаты х у через временные из цикла
+            cell = maxScoreFieldCell;
+        }
     }
 }
